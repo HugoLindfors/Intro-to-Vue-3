b65eb3bf888870ca55423cc7ec3046fa6017d94e
Hugo Lindfors <hugo.lindfors2@outlook.com>
2024-10-26 17:16:18 +0200

    L2-start finished

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Astrophysics for People in a Hurry',
+            description: 'Astrophysics for People in a Hurry is a 2017 popular science book by Neil deGrasse Tyson, centering around a number of basic questions about the universe.',
+            dateOriginallyPublished: 'May 2, 2017',
+            author: 'Neil deGrasse Tyson',
+            genre: 'Non-fiction',
+            isbn: '978-0-393-60939-4',
+            numberOfPages: 244
+        }
+    }
+})
